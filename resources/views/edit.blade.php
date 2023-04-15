b57552abd4bea8c6e083245fc96d14363cefdac7
jahc <jahc@hotmail.com>
2023-04-14 22:31:52 -0600

    Esto es hecho desde mi casa espero y funciones

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Esto es hecho desde mi casa</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Pues desde mi9 casa se hace esta rama</h1>
+    </div>
+</body>
+</html>
