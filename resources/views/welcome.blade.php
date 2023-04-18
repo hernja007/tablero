96f6cbf02540ce388e463e53690a3e4860f9ecab
jahc <jahc666@hotmail.com>
2023-04-18 15:30:15 -0600

    Una ultima cosa que si me gustaria que saliera bien

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,14 @@
     <div class="container">
         asi dejemos este desmadre
     </div>
+    <div class="container">
+        Uno más pero todo verde
+    </div>
+    <div class="container">
+        Uno menos pero todo verde
+    </div>
+    <div class="container">
+        pues ahi se hace pero todo verde
+    </div>
 </body>
 </html>
